fd8a7925852c3d061f05e54f85aff2947563f3c3
averi4113 <205599851+averi4113@users.noreply.github.com>
2025-04-07 16:53:24 +0800

    Added Buttons on End Page Section

--- src/components/End Page Section/EndPageSection.jsx
+++ src/components/End Page Section/EndPageSection.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import '../../styles/EndPageSectionStyles.css' 
-
+import logo from '../../assets/averi-logo-nobg1.png'
 const EndPageSection = () => {
   return (
     <>
-    
     <div className='endpage-container'>
-    <p className='endpage-1'> &gt;since you’re here, check out my work!</p>
-    <p className='endpage-1'> &gt;you can also check out my blogs!</p>
-      </div>
-</>
+      <p className='endpage-1'> &gt;since you're here, check out my work!</p>
+      <button className='endpage-button'>my work</button>
+      <p className='endpage-1'> &gt;you can also check out my blogs!</p>
+      <button className='endpage-button-2'>my blogs</button>
+    </div>
+    <div className='logo-endpage-container'>
+      <img src={logo} alt="Logo" className="logo-endpage" /> </div>
+    </>
   )
 }
 
